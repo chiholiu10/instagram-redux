78b3edb8bcf1d47ff8305b63a2dfb1b74340e14f
chiho liu <chiho@192.168.2.6>
2020-01-14 21:06:25 +0100

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,39 @@
+export const types = {
+
+}
+
+export const addPhoto = () => {
+
+}
+
+export const likePhoto = () => {
+    
+}
+
+export const commentPhoto = () => {
+    
+}
+
+export const editCommentPhoto = () => {
+    
+}
+
+export const removeCommentPhoto = () => {
+    
+}
+
+export const replyCommentPhoto = () => {
+    
+}
+
+export const likeCommentPhoto = () => {
+    
+}
+
+
+
+
+likeCommentPhoto
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App.js';
+import { Provider } from 'react-redux';
+import todo from './reducers';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const store = createStore(
+    todo,
+    applyMiddleware(thunk)
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Instagram = () => (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+)
+
+ReactDOM.render(<Instagram/>, document.getElementById('root'));
+registerServiceWorker();
